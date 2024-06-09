137d9c517b9d5cc4b415faaa0aaac51db288bb4d
liang <445809072@qq.com>
2024-06-10 00:04:30 +0200

    lamp work main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,30 @@
 #include <Arduino.h>
 
+bool pin_6_now;
+bool pin_6_last;
+bool zero_crossing_flag;
 void setup() {
   pinMode(14, OUTPUT);//LED
+  pinMode(6, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(1, INPUT);//key2
+  pinMode(0, INPUT);//key1
+  pin_6_last=0;
+  pin_6_now=0;
+  zero_crossing_flag=0;
 }
 
 void loop() {
-  digitalWrite(14, HIGH);
-  delay(1000);
-  digitalWrite(14, LOW);
-  delay(1000);
+  pin_6_last=pin_6_now;
+  pin_6_now=digitalRead(6);
+
+  if(pin_6_last==LOW&&pin_6_now==HIGH){
+    zero_crossing_flag=1;
+  }
+  if(zero_crossing_flag){
+    digitalWrite(3,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(3,LOW);
+    zero_crossing_flag=0;
+  }
 }
